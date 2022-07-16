934f8e83d167ed1c11842319dc9b2bb2c6bc2c5c
Jonatana RANDRIAMPARANY <103509534+Jonas4884@users.noreply.github.com>
2022-07-16 22:51:35 +0300

    Finalize Python-API

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
 from fastapi import FastAPI
-from scapper import Scraper
-from fastapi import FastAPI
 import asyncio
 import uvicorn
+from typing import List
+from pydantic import Json
+from requests_html import HTMLSession;
 
+class Scraper():
+    def scrapedata(self,tag ):
+        url = f'https://www.portaljob-madagascar.com/emploi/liste/secteur/informatique-web/page/{tag}'
+        s = HTMLSession()
+        ro = s.get(url)
+        quotes = ro.html.find('aside.contenu_annonce')
+        qlist = []
+        
+        for q in quotes:
+            item = {
+                
+            'text':q.find('strong',first = True).text.strip(),
+            'title': q.find('h4',first = True).text.strip(),
+            'Contrat':q.find('h5',first= True).text.strip(),
+            'description':q.find('a.description',first = True).text.strip(),
+            'image':q.find('img',first = True)
+            }
+            
+            qlist.append(item)
+            print(qlist)
+        return qlist
 async def main():
     config = uvicorn.Config("main:app", port=5000, log_level="info")
     server = uvicorn.Server(config)
