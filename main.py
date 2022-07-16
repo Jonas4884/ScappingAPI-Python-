7ca201132b3826b78f28258c6a14e684d5974df6
Jonatana RANDRIAMPARANY <103509534+Jonas4884@users.noreply.github.com>
2022-07-16 21:26:34 +0300

    fixing app

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 from scapper import Scraper
 from fastapi import FastAPI
+import asyncio
+import uvicorn
+
+async def main():
+    config = uvicorn.Config("main:app", port=5000, log_level="info")
+    server = uvicorn.Server(config)
+    await server.serve()
+
+if __name__ == "__main__":
+    asyncio.run(main())
 
 app = FastAPI()
 quotes = Scraper()
